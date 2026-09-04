511bda732c2752c724dca427b1914d21d72066a1
kianalovells2004-eng <kianalovells2004@gmail.com>
2026-09-04 01:26:26 -0700

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -1,1 +1,15 @@
+local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/kianalovells2004-source/Uni/refs/heads/main/esp.lua"))()
 
+local library = loadstring(game:GetObjects("rbxassetid://7657867786")[1].Source)("Pepsi's UI Library")
+local window = library:CreateWindow({ Name = "ESP Hub" })
+local espTab = window:CreateTab({ Name = "ESP" })
+local section = espTab:CreateSection({ Name = "Visuals", Side = "Left" })
+
+section:AddToggle({
+    Name = "Box ESP",
+    Value = false,
+    Flag = "BoxESP",
+    Callback = function(value)
+        ESP:Toggle(value)
+    end
+})
